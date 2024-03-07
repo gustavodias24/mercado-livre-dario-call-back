16f31629c29be15105e79a4aa99927dca25e4515
gusta <gustavo024dias@gmail.com>
2024-03-07 10:30:12 -0300

    gerando token

--- index.py
+++ index.py
@@ -1,16 +1,30 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    json = request.json
-    args = request.args
+    code = request.args.get('code')
+    url = 'https://api.mercadolibre.com/oauth/token'
 
-    json.update(args)
+    payload = {
+        'grant_type': 'authorization_code',
+        'client_id': '200629904312027',
+        'client_secret': 'EbCGNmbnX74KGjkaqe8FS4cv4npZqMDt',
+        'code': code,
+        'redirect_uri': 'https://mercado-livre-dario-call-back.vercel.app/',
+    }
 
-    return jsonify(json)
+    headers = {
+        'accept': 'application/json',
+        'content-type': 'application/x-www-form-urlencoded'
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+
+    return jsonify(response.json())
 
 
 if __name__ == "__main__":
